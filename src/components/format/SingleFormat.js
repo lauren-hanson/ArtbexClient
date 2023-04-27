a43e9e1ddcad061a1daa77caddb04e2d6dced341
Lauren Hanson <lohanson33@gmail.com>
2023-04-26 21:07:13 -0700

    photos & creation area rendering

---
+++ src/components/format/SingleFormat.js
@@ -0,0 +1,6 @@
+
+export const SingleFormat = () => { 
+
+    return (<>
+     ONE </>)
+}
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,12 @@
-import { useCallback, useState } from "react"
 // import { ToneList } from "../tone/ToneList"
-// import { Dropzone } from "../dropzone/Dropzone"
 import { FormatList } from "../format/FormatList"
 import { Box } from "../box/Box"
-import cuid from "cuid"
 
 export const Home = () => {
 
-    const [images, setImages] = useState([])
-
-    const onDrop = useCallback(acceptedFiles => {
-        acceptedFiles.map(file => {
-            const reader = new FileReader()
-
-            reader.onload = function (e) {
-                setImages(prevState => [
-                    ...prevState,
-                    { id: cuid(), imageUrl: e.target.result }
-                ])
-            }
-            reader.readAsDataURL(file);
-            return file;
-        })
-    }, []);
 
     return (<>
         <h2>ArtBex</h2>
-        {/* <Dropzone onDrop={onDrop} accept={'image/*'} /> */}
         {/* <ToneList /> */}
         <FormatList />
         <Box />
