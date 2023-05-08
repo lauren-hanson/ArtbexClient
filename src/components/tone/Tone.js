ac97e8c22d711374b88a3ae8b129507bf9da3b08
Lauren Hanson <lohanson33@gmail.com>
2023-05-08 12:36:03 -0700

    images rendering as empty boxes

--- src/components/tone/Tone.js
+++ src/components/tone/Tone.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useDrag } from "react-dnd";
 
-export const Tone = ({ id, image }) => {
+export const Tone = ({ id, src, text }) => {
     const [{ isDragging }, drag] = useDrag(() => ({
-        type: "images",
+        type: "IMAGE",
         item: { id: id },
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging(),
         }),
     }))
+
     return (
         <>
             <img
                 ref={drag}
-                src={image}
+                src={src}
+                // alt={text}
                 width="150px"
                 style={{ border: isDragging ? "5px solid pink" : "0px" }}
             />
